fc4025c4af9367e7ab270222dbed37ebfe3235f4
jemima139 <165708488+jemima139@users.noreply.github.com>
2024-06-10 09:55:26 -0400

    Updated

--- index.js
+++ index.js
@@ -8,27 +8,23 @@ app.use(bodyParser.json());
 
 let mockDatabase = [];
 
-
-const setTimeout= (response, delay) => {
-    return new Promise(resolve => setTimeout(() => resolve(response), delay));
+const delayResponse = (data, delay) => {
+    return new Promise(resolve => setTimeout(() => resolve(data), delay));
 };
 
-
 app.get('/api/data', async (req, res) => {
-    const data = setTimeout(() => res.json(mockDatabase[index]), 500); 
+    const data = await delayResponse(mockDatabase, 500);
     res.json(data);
 });
-
 
 app.post('/api/data', async (req, res) => {
     const newData = req.body;
     newData.id = mockDatabase.length ? mockDatabase[mockDatabase.length - 1].id + 1 : 1;
     mockDatabase.push(newData);
     
-    setTimeout(() => res.json(mockDatabase[index]), 500);
-    res.status(201).json(newData);
+    const data = await delayResponse(newData, 500);
+    res.status(201).json(data);
 });
-
 
 app.put('/api/data/:id', async (req, res) => {
     const id = parseInt(req.params.id);
@@ -36,21 +32,20 @@ app.put('/api/data/:id', async (req, res) => {
     const index = mockDatabase.findIndex(item => item.id === id);
     if (index !== -1) {
         mockDatabase[index] = { ...mockDatabase[index], ...updateData };
-        
-        setTimeout(() => res.json(mockDatabase[index]), 1000);
+        const data = await delayResponse(mockDatabase[index], 1000);
+        res.json(data);
     } else {
         res.status(404).json({ error: 'Data not found' });
     }
 });
-
 
 app.delete('/api/data/:id', async (req, res) => {
     const id = parseInt(req.params.id);
     const index = mockDatabase.findIndex(item => item.id === id);
     if (index !== -1) {
         const deletedData = mockDatabase.splice(index, 1);
-        setTimeout(() => res.json(mockDatabase[index]), 500);
-        res.json(deletedData[0]);
+        const data = await delayResponse(deletedData[0], 500);
+        res.json(data);
     } else {
         res.status(404).json({ error: 'Data not found' });
     }
